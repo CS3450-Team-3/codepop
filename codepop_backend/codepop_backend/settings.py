0815f648d342daff01187272fe083c176be985b9
Michael S. <michaelbseeley22@gmail.com>
2026-03-22 18:29:35 -0600

    Added Store Manager Dashboard w/o Auth

--- codepop_backend/codepop_backend/settings.py
+++ codepop_backend/codepop_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'drf_spectacular',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,6 +86,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'codepop_backend.wsgi.application'
 
+# Custom User Model
+AUTH_USER_MODEL = 'backend.CustomUser'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
---
+++ codepop_frontend/app/[store]/layout.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function StoreLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
